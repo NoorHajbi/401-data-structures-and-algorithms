471988e691e39bd32fde6d646a015a929e4e579d
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-07-11 23:52:01 +0300

    initial commit

---
+++ challenges/stackQueue/app/src/main/java/stackQueue/pseudoQueue.java
@@ -0,0 +1,42 @@
+package stackQueue;
+
+public class pseudoQueue {
+    StackLinkedList<Integer> stackList1;
+    StackLinkedList<Integer> stackList2;
+
+    pseudoQueue() {
+        this.stackList1 = new StackLinkedList<Integer>();
+        this.stackList2 = new StackLinkedList<Integer>();
+    }
+//    Methods:
+//    1
+//    input [10]->[15]->[20]
+//          arg  5
+//            output [5]->[10]->[15]->[20]
+//    (2)
+//    5
+//    5
+//    enqueue
+//    Arguments: value
+//    Inserts value into the PseudoQueue, using a first-in, first-out approach.
+
+//    (1)
+//   input [5]->[10]->[15]->[20]
+//        output    20
+//           state [5]->[10]->[15])
+
+//    2
+//    [5]->[10]->[15]
+//    15
+//5 10
+//    dequeue
+//    Arguments: none
+//    Extracts a value from the PseudoQueue, using a first-in, first-out approach.h
+
+
+
+//    https://codefellows.github.io/common_curriculum/data_structures_and_algorithms/Challenge_Testing
+//    https://codefellows.github.io/common_curriculum/data_structures_and_algorithms/Challenge_Documentation
+//    https://codefellows.github.io/common_curriculum/data_structures_and_algorithms/Challenge_Submission
+
+}
