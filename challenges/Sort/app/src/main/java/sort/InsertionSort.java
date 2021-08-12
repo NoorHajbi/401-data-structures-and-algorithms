b1fc995cbb2b8c15b55fedc076b5821d1c69a12e
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-08-12 06:29:51 +0300

    initial commit

---
+++ challenges/Sort/app/src/main/java/sort/InsertionSort.java
@@ -0,0 +1,42 @@
+package sort;
+
+public class InsertionSort {
+    /**
+     * Selection Sort is a sorting algorithm
+     * The traversal keeps track of the minimum value and places it in the front of the array
+     * which should be incrementally sorted.
+     **/
+    public static int[] selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i <= n; i++) {
+            int j = i - 1;
+            int temp = arr[i];
+
+            while (j >= 0 && temp < arr[j]) {
+                arr[j + 1] = arr[j];
+                j -= 1;
+            }
+
+            arr[j + 1] = temp;
+        }
+        return arr;
+    }
+
+    public static int[] selectionSortDescending(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i <= n; i++) {
+            int j = i - 1;
+            int temp = arr[i];
+
+            while (j >= 0 && temp > arr[j]) {
+                arr[j + 1] = arr[j];
+                j -= 1;
+            }
+
+            arr[j + 1] = temp;
+        }
+        return arr;
+    }
+
+}
+
---
+++ challengesRead/Sort/BLOG.md
@@ -0,0 +1,26 @@
+# Sorting Algorithms
+
+- In computer science, a sorting algorithm is an algorithm that puts elements of a list into an order. 
+
+# Insertion(Selection) Sort
+Selection Sort is a sorting algorithm that traverses the array multiple times finding the ascending/descending order.
+
+# Challenge Summary
+<!-- Description of the challenge -->
+ sorting an Array of elements from the beginning to end. 
+ 
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+
+
+# Resourses: 
+1. [Sorting Algorithm](https://en.wikipedia.org/wiki/Sorting_algorithm).
+2. [Insertion Sort - Video](https://www.youtube.com/watch?v=JecAk1FAOck).
+3. [Insertion Sort - example document](https://codefellows.github.io/common_curriculum/data_structures_and_algorithms/Code_401/class-26/solutions/BLOG)
