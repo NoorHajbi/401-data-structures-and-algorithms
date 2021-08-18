ea7e8ba1f3eb3fa7a6cf1672f3299c21beae85a3
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-08-18 06:50:36 +0300

    add node class

---
+++ challenges/hashTable/app/src/main/java/hashTable/data/Node.java
@@ -0,0 +1,44 @@
+package hashTable.data;
+
+public class Node<K,V> {
+    K key;
+    V value;
+
+    final int hashCode;
+    Node<K,V> next;
+
+
+    public Node(K key, V value, int hashCode) {
+        this.key = key;
+        this.value = value;
+        this.hashCode = hashCode;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public int getHashCode() {
+        return hashCode;
+    }
+
+    public Node<K, V> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<K, V> next) {
+        this.next = next;
+    }
+}
