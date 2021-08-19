a54d047d7720b88b05756fbfb56580cb42953072
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-08-19 04:44:40 +0300

    finish merge sort

--- challenges/Sort/app/src/test/java/sort/MergeSortTest.java
+++ challenges/Sort/app/src/test/java/sort/MergeSortTest.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
-public class QuickSortTest {
+public class MergeSortTest {
     int[] arr = {15, 2, 20, 12, 4};
     int[] sortedArr = {1, 2, 3, 4, 5};
 
@@ -14,13 +14,13 @@ public class QuickSortTest {
     int[] fewUniques = {5, 12, 7, 5, 5, 7};
     int[] nearlySorted = {2, 3, 5, 7, 13, 11};
 
-    QuickSort sort = new QuickSort();
+    MergeSort sort = new MergeSort();
 
     //1. Empty Array
     @Test
     public void testEmpty() {
         int[] arrEmpty = {};
-        sort.quickSort(arrEmpty);
+        sort.mergeSort(arrEmpty);
         assertNotNull("Problem with sorting empty array", Arrays.toString(arrEmpty));
     }
 
@@ -28,14 +28,14 @@ public class QuickSortTest {
     @Test
     public void testOneElement() {
         int[] arrOneElement = {1};
-        sort.quickSort(arrOneElement);
+        sort.mergeSort(arrOneElement);
         assertEquals("[1]", Arrays.toString(arrOneElement));
     }
 
     //    3. Sorted Array
     @Test
     public void testSortedArray() {
-        sort.quickSort(sortedArr);
+        sort.mergeSort(sortedArr);
         assertEquals("[1, 2, 3, 4, 5]", Arrays.toString(sortedArr));
     }
 
@@ -43,7 +43,7 @@ public class QuickSortTest {
     @Test
     public void testSort() {
         //Ascending
-        sort.quickSort(arr);
+        sort.mergeSort(arr);
         assertEquals("[2, 4, 12, 15, 20]", Arrays.toString(arr));
     }
 
@@ -51,15 +51,15 @@ public class QuickSortTest {
     @Test
     public void happyPath() {
         //1.
-        sort.quickSort(reverseSorted);
+        sort.mergeSort(reverseSorted);
         assertEquals("[-2, 5, 8, 12, 18, 20]", Arrays.toString(reverseSorted));
 
         //2.
-        sort.quickSort(fewUniques);
+        sort.mergeSort(fewUniques);
         assertEquals("[5, 5, 5, 7, 7, 12]", Arrays.toString(fewUniques));
 
         //3.
-        sort.quickSort(nearlySorted);
+        sort.mergeSort(nearlySorted);
         assertEquals("[2, 3, 5, 7, 11, 13]", Arrays.toString(nearlySorted));
 
     }
